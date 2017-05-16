0d51ed7730f6d251955596de65f69e3d813f060a
Seth T <seth10@users.noreply.github.com>
2017-05-16 11:30:02 -0400

    Add shellinabox to setup.sh

--- setup/setup.sh
+++ setup/setup.sh
@@ -71,7 +71,7 @@ sudo cp /tmp/config.txt /boot/config.txt
 echo "Updating installations files. This may take several minutes..."
 sudo apt-get update -qq -y
 echo "Installing packages..."
-sudo apt-get install -qq mpg123 build-essential python-dev python-smbus python-pip python-imaging python-numpy python-matplotlib python-scipy git nmap -y
+sudo apt-get install -qq mpg123 build-essential python-dev python-smbus python-pip python-imaging python-numpy python-matplotlib python-scipy git nmap openssl shellinabox -y
 sudo apt-get install -qq python-opencv -y
 sudo apt-get install -qq apache2 php5 libapache2-mod-php5 -y
 sudo pip install -qq flask
